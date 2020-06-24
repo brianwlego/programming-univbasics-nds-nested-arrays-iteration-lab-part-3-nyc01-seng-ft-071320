5e4a06c422ec2c4c419d9a112c4f0d0d7efdb309
Brian Lego <legobw@gmail.com>
2020-06-24 15:46:45 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,7 +4,13 @@ def join_nested_strings(src)
   
   row_index = 0 
   array_of_strings = []
-  
-  
-  
+  while row_index<src.count do
+    element_index = 0 
+    while element_index<src[row_index].count do
+        src[row_index][element_index].type
+      element_index += 1 
+    end   
+    row_index += 1
+  end  
+  return array_of_strings.join(' ')
 end
